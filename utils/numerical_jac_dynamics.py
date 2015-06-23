8eb4ecb6f506ae39a87a69d520d886d4487ee1b7
Jennifer Shih <jennifershih@Jennifers-MacBook-Pro-4.local>
2015-06-23 15:15:52 -0700

    added helper file for computing numerical jacobian for dynamics

---
+++ utils/numerical_jac_dynamics.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def numerical_jac_dynamics(f, x, u, dt, eps=pow(10, -5)):
+    """
+    Compute the numerical jacobian for dynamics f using
+    current trajectory x and control u
+    (TODO) test that this function is correct
+    """
+    y = f(x) 
+    assert len(x) == len(u)
+    grad_x = np.zeros((len(y), len(x)));
+    grad_u = np.zeros((len(y), len(u)));
+    
+    xp = x.copy()
+
+    for i in range(len(x)):
+        xp[i] = x[i] + eps / 2
+        yhi = f(xp, u, dt)
+        xp[i] = x[i] - eps / 2
+        ylow = f(xp, u, dt)
+        xp[i] = x[i]
+        grad_x[:,i] = (yhi - ylow) / eps
+    
+    up = u.copy()
+
+    for i in range(len(u)):
+        up[i] = u[i] + eps / 2
+        yhi = f(x, up, dt)
+        up[i] = u[i] - eps / 2
+        ylow = f(x, up, dt)
+        up[i] = u[i]
+        grad_u[:,i] = (yhi - ylow) / eps
+
+    return (grad_x, grad_u)
