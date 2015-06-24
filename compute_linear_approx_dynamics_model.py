a1163e96e6b4f1c7a9ad4370eeb48174d981ebb4
jennifershih <jennifershih@Johnbishek.(none)>
2015-06-24 01:10:48 -0700

    fix bug in linear approx dynamics model

--- compute_linear_approx_dynamics_model.py
+++ compute_linear_approx_dynamics_model.py
@@ -5,15 +5,16 @@ def compute_linear_approx_dynamics_model(f, curr_X, curr_U, dt, eps=pow(10, -5))
     T = curr_X.shape[1]
     assert curr_X.shape[1] == curr_U.shape[1]
     
-    linearized_X = np.empty((T - 1, len(curr_X), len(curr_X)))
-    linearized_U = np.empty((T - 1, len(curr_X), len(curr_U)))
+    linearized_X = np.empty((T, len(curr_X), len(curr_X)))
+    linearized_U = np.empty((T, len(curr_X), len(curr_U)))
 
-    # (TODO) parallize this
-    for i in range(T-1):
+    # (TODO) parallize this 
+    # (TODO) this can also be faster using row major
+    for i in range(T)
         x = curr_X[:,i]
         u = curr_Y[:,i]
         (jac_x, jac_u) = numerical_jac_dynamics(f, x, u, dt, eps)
-        linearized_X[:,i] = jac_x
-        linearized_U[:,i] = jac_u
+        linearized_X[i] = jac_x
+        linearized_U[i] = jac_u
 
     return (linearized_X, linearized_U)
