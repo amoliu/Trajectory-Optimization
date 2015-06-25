43da52f47647fb6ae3443efc14c1e5cbde481e09
jennifershih <jennifershih@Johnbishek.(none)>
2015-06-24 23:18:47 -0700

    verified most of the implementation in ilqr

--- ilqr.py
+++ ilqr.py
@@ -1,10 +1,10 @@
 import numpy as np
 from compute_linear_approx_dynamics_model import compute_linear_approx_dynamics_model
-from compute_quadratic_approx_cost import compute_quadratic_approx_cost
-from compute_quadratic_approx_cost_pendulum import compute_quadratic_approx_cost_pendulum
+# from compute_quadratic_approx_cost import compute_quadratic_approx_cost
+# from compute_quadratic_approx_cost_pendulum import compute_quadratic_approx_cost_pendulum
 
 class iLQR(object):
-    def __init__(self, f, T, dt, init_state, control_init, cost_func, traj_init=None, Q=None, Q_f=None):
+    def __init__(self, f, T, dt, init_state, control_init, quadratic_approx_cost_func, traj_init=None, Q=None, Q_f=None):
         """
         Initialize constants
         """
@@ -16,7 +16,7 @@ class iLQR(object):
         self.traj_init = traj_init
         self.Q = Q
         self.Q_f = Q_f
-        self.cost_func = cost_func
+        self.quadratic_approx_cost_func = quadratic_approx_cost_func
 
     def execute_control(self, f, x_init, U, dt):
         """
@@ -56,7 +56,7 @@ class iLQR(object):
         dt = self.dt
         Q = self.Q
         Q_f = self.Q_f
-        cost_func = self.cost_func
+        quadratic_cost_func = self.quadratic_approx_cost_func
 
         curr_X = np.zeros((T+1, len(self.init_state)))
         curr_X[0] = self.init_state
@@ -67,7 +67,7 @@ class iLQR(object):
         nX = len(self.init_state)
         nU = curr_U.shape[1]
 
-        max_iter = 10
+        max_iter = 20
         iteration = 0
         
         while not converged and iteration < max_iter:
@@ -80,12 +80,12 @@ class iLQR(object):
             # a second order Taylor expansion of the cost function
             #
             ## First order taylor approximation of the dynamics model
-            A_X, B_U = compute_linear_approx_dynamics_model(f, curr_X, curr_U, self.dt)
+            A_X, B_U = compute_linear_approx_dynamics_model(f, curr_X, curr_U, self.dt) # ok (might want to double check)
 
             ## Second order Taylor expansion of the cost function
             ## (TODO) Use an actual approximation and use the below function instaed
             # Q_X, R_U = compute_quadratic_approx_cost(curr_X, curr_U)
-            Q_X, R_U = compute_quadratic_approx_cost_pendulum(curr_X, curr_U, Q_f, Q)
+            Q_X, R_U = quadratic_cost_func(curr_X, curr_U, Q_f, Q) # ok
 
             # Use the LQR backups to solve for the optimal control policy for LQ approximation obtained
             # in previous state
@@ -95,47 +95,46 @@ class iLQR(object):
             """
             Write out the equations here:
             """
-            Q_N = Q_X[-1]
-            V_N = Q_N.dot(curr_X[-1])
+            Q_N = Q_X[-1] #ok
+            V_N = Q_N.dot(curr_X[-1]) #ok
 
-            S = np.zeros((T+1, nX, nX))
-            S[-1] = Q_N
+            S = np.zeros((T+1, nX, nX)) #ok
+            S[-1] = Q_N #ok
 
-            V = np.zeros((T+1, nX))
-            V[-1] = V_N
+            V = np.zeros((T+1, nX)) #ok
+            V[-1] = V_N #ok
 
-            R = R_U[0] #(TODO)
-            Q = Q_X[0] #(TODO)
+            R = R_U[0] #ok
+            Q = Q_X[0] #ok
 
-            DELTA_U = np.zeros((T, nU))
+            DELTA_U = np.zeros((T, nU)) #ok
 
             for i in range(T):
-                index = T - i - 1
+                index = T - i - 1 #ok
 
-                x = curr_X[index]
-                u = curr_U[index]
+                x = curr_X[index] #ok
+                u = curr_U[index] #ok
 
-                A = A_X[index]
-                B = B_U[index]
+                A = A_X[index] #ok
+                B = B_U[index] #ok
 
-                S_n = S[index+1]
-                K_help = np.linalg.inv(B.T.dot(S_n).dot(B) + R)
-                K = K_help.dot(B.T).dot(S_n).dot(A)
+                S_n = S[index+1] #ok
+                K_help = np.linalg.inv(B.T.dot(S_n).dot(B) + R) #ok
+                K = K_help.dot(B.T).dot(S_n).dot(A) #ok
 
-                S_k = A.T.dot(S_n).dot(A - B.dot(K)) + Q
-                S[index] = S_k
+                S_k = A.T.dot(S_n).dot(A - B.dot(K)) + Q #ok
+                S[index] = S_k #ok
 
-                K_v = K_help.dot(B.T)
-                K_u = K_help.dot(R)
+                K_v = K_help.dot(B.T) #ok
+                K_u = K_help.dot(R) #ok
 
-                v_n = V[index+1]
-                v = (A - B.dot(K)).dot(v_n) - K.T.dot(R).dot(u) + Q.dot(x)
+                v_n = V[index+1] #ok
+                v = (A - B.dot(K)).T.dot(v_n) - K.T.dot(R).dot(u) + Q.dot(x) #ok
                 V[index] = v
 
-                # delta_x =  (TODO) Figure this out and double check this??
-                delta_x = curr_X[index+1] - x
-                delta_u = -K.dot(delta_x) - K_v.dot(v_n) - K_u.dot(u)
-                DELTA_U[index] = delta_u
+                delta_x = x
+                delta_u = -K.dot(delta_x) - K_v.dot(v_n) - K_u.dot(u) #ok
+                DELTA_U[index] = delta_u #ok
 
             curr_U = curr_U + DELTA_U # (TODO) Is this correct??
 
@@ -147,6 +146,7 @@ class iLQR(object):
                 abs_diff_U = np.sum(np.sum(abs(diff_U), axis=1), axis=0)
                 abs_diff = abs_diff_X + abs_diff_U
                 print("abs_diff_U: {}".format(abs_diff_U))
+                raw_input("look at difference ")
 
                 if abs_diff < threshold:
                     converged = True
@@ -156,7 +156,7 @@ class iLQR(object):
             
             iteration += 1
 
-        last_state = curr_X[-1]
+        final_state = curr_X[-1]
         print("The final state is {}".format(final_state))
 
         return curr_U
